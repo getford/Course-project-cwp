884d82176c87fc4ab0fe1618eae8216d21f56e3a
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-03-28 08:40:30 +0300

    add genkey function

--- Metrica/index.js
+++ Metrica/index.js
@@ -2,22 +2,27 @@ const express = require('express');
 const Sequelize = require('sequelize');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const Promise = require("bluebird");
 
 const config = require('./config');
 
 const dbcontext = require('./context/db')(Sequelize, config);
+const metrica = require('./metrica');
 
 const app = express();
 
-
-app.get('/', function (req, res) {
-    res.send('Welcome to official site for Metrica!');
-});
 
 console.log("http://localhost:3000");
 dbcontext.sequelize
     .sync()
     .then(() => {
+        app.get('/', function (req, res) {
+            res.send('Welcome to official site for Metrica!');
+        });
+
+        app.get('/genkey', function (req, res) {
+            metrica.genSK();
+        });
         app.listen(3000, () => console.log('Running'));
     })
     .catch((err) => console.log(err));
