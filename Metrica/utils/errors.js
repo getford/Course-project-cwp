a5ba2b563a15869bcfe2b38c11fb37a61904e548
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-04-21 20:58:07 +0300

    add list errors

--- Metrica/utils/errors.js
+++ Metrica/utils/errors.js
@@ -3,40 +3,80 @@ const express = require('express');
 express.response.error = (error) => {
     if (!error.code) {
         error = {
-            message: 'error, please try again',
-            code: 'undefined_error',
-            status: 400
+            message: 'Internal Server Error',
+            code: 'Internal_Server_Error',
+            status: 500
         };
     }
-    this.status(error.status).json(error);
+    else
+        return error.code;
+        // this.error(error.status).JSON.error(error);
+    /*тут изменить*/
 };
 
 module.exports = {
-    invalidId: {
+    noContent: {        // нет данных для отправки клиенту
+        message: 'No content',
+        code: 'no_content',
+        status: 204
+    },
+
+    // ошибки клиента
+    badRequest: {   // Запрос не может быть исполнен ввиду синтаксической ошибки.
         message: 'Bad request',
         code: 'bad_request',
         status: 400
     },
-    notFound: {
-        message: 'Entity not found',
-        code: 'entity_not_found',
-        status: 404
-    },
-    wrongCredentials: {
-        message: 'Email or password are wrong',
-        code: 'wrong_credentials',
-        status: 401
-    },
-    accessDenied: {
-        message: 'Access denied',
-        code: 'access_denied',
-        status: 403
-    },
-    Unauthorized: {
+    unauthorized: { // указывает на то, что попытка авторизации была отклонена
         message: 'Unauthorized',
         code: 'unauthorized',
         status: 401
     },
+    forbidden: {    // нет доступа, авторизация не поможет
+        message: 'Forbidden',
+        code: 'forbidden',
+        status: 403
+    },
+    notFound: {
+        message: 'Entity not found',
+        code: 'not found',
+        status: 404
+    },
+    methodNotAllowed: { // Метод, при помощи которого совершается запрос к ресурсу,
+        // не доступен.
+        message: 'Method Not Allowed',
+        code: 'method_not_allowed',
+        status: 405
+    },
+    requestEntityTooLarge: { // сервер отказывается обработать запрос по причине
+        // слишком большого размера тела запроса.
+        message: 'Request Entity Too Large',
+        code: 'request_entity_too_large',
+        status: 413
+    },
+    tooManyRequests: {  // клиент попытался отправить слишком много запросов за
+        // короткое время.
+        message: 'Too Many Requests',
+        code: 'too_many_requests',
+        status: 429
+    },
+
+    // ошибки сервера
+    notImplemented: {   // Сервер либо не понимает метод в запросе,
+        // либо не поддерживает возможностей, необходимых для обработки запроса
+        message: ' Not Implemented',
+        code: 'not_implemented',
+        status: 501
+    },
+    badGateway: {   // когда сервер, выступая в роли шлюза или прокси-сервера,
+        // получил недействительное ответное сообщение от вышестоящего сервера.
+        message: 'Bad Gateway',
+        code: 'bad_gateway',
+        status: 502
+    },
+
+
+    // для резработчика
     DatabaseError: {
         message: 'Data base have some errors',
         code: 'db_error',
