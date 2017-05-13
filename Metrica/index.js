71db78aacc84ae652bd64c103581e8bf575142b1
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-05-13 05:38:33 +0300

    add new files for UI

--- Metrica/index.js
+++ Metrica/index.js
@@ -11,7 +11,6 @@ const logger = require('./utils/logger');
 
 const db = require('./context/db')(Sequelize, config);
 
-
 const authService = require('./services/auth')(db.auth, db.site, db.gotourl, errors);
 const siteService = require('./services/site')(db.site, db.gotourl, errors);
 const gotourlService = require('./services/gotourl')(db.gotourl, db.site, db.auth, errors);
@@ -27,10 +26,13 @@ const cache = require('./utils/cache')(cacheService);
 
 const app = express();
 
+let date = new Date();
+
 app.use(express.static('public'));
 app.use(cookieParser(config.cookie.key));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/api', logger);
 app.use('/api', auth);
 app.use('/api', cache);
@@ -52,15 +54,18 @@ app.get('/manage.html', (req, res) => {
     res.sendFile(path.resolve(__dirname + "/public/html/manage.html"));
 });
 
-console.log("http://localhost:" + config.port);
+console.log("http://localhost:" + config.port + "\n");
 db.sequelize
     .sync()
     .then(() => {
         app.listen(config.port, () => {
+            console.log("date:\t" + date.getDate() +
+                "." + (date.getMonth() + 1) +
+                "." + date.getFullYear());
             console.log("host:\t" + config.db.host);
             console.log("name:\t" + config.db.name);
             console.log("user:\t" + config.db.user);
-            console.log('--- Success ---\n');
+            console.log('\n--- Success ---\n');
         });
     })
     .catch((err) => console.log(err));
--- Metrica/services/error.js
+++ Metrica/services/error.js
@@ -4,11 +4,37 @@ const Promise = require("bluebird");
 let date = new Date();
 
 module.exports = (errorRepository, siteRepository, errors) => {
-    return {catchErrors: catchErrors};
+    return { catchErrors: catchErrors };
 
     function catchErrors(data) {
         return new Promise((resolve, reject) => {
+            userRepository.findOne({
+                where: { key: data.key },
+                attributes: ['id', 'key']
+            })
+                .then((resultUR) => {
+                    if (resultUR.key === data.key) {
+                        siteRepository.findOne({
+                            where: { url: data.url, authId: resultUR.id },
+                            attributes: ['id', 'authId']
+                        })
+                            .then((resultSR) => {
+                                if (resultSR.authId === resultUR.id) {
+                                    errorRepository.findOne({
+                                        where: {},
+                                        attributes: []
+                                    })
+                                        .then(() => { })
+                                        .catch(() => { })
+                                }
+                                else {
 
+                                }
+                            })
+                            .catch(() => { reject(errors.notFound); });
+                    }
+                })
+                .catch(() => { reject(errors.notFound); });
         })
     }
 };
