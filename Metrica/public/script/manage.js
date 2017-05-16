371dab0e0bfd4f58436d1d5cbb90af57fd33badc
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-05-17 01:59:55 +0300

    v3.0.1

--- Metrica/public/script/manage.js
+++ Metrica/public/script/manage.js
@@ -75,31 +75,52 @@ function drawRow(rowData) {
     let row = $("<tr />");
     $("#mysitetable").append(row);
     row.append($("<td id='urlClick'>" + "<span onclick=\"myGotoUrlCount('" + rowData.url + "\')\">" + rowData.url + "</span></td>"));
+
 }
 
 function myGotoUrlCount(dataUrl) {
+    let jsonData = JSON.stringify({url: dataUrl});
 
-    let jsonData = JSON.stringify({
-        url: dataUrl
-    });
+    if ($("#thisDate").attr('checked', 'checked')) {
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:3000/api/gotourl/infourls"); // async=true
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhr.send(jsonData);
-    xhr.onload = function (e) {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let graphData = JSON.parse(xhr.responseText);
-            Morris.Bar({
-                element: 'bar-example',
-                data: graphData,
-                xkey: 'url',
-                ykeys: ['count'],
-                labels: ['Count']
-            });
-            return xhr.responseText;
-        }
-    };
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:3000/api/gotourl/infourls"); // async=true
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.send(jsonData);
+        xhr.onload = function (e) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                let graphData = JSON.parse(xhr.responseText);
+                Morris.Bar({
+                    element: 'bar-urls',
+                    data: graphData,
+                    xkey: 'url',
+                    ykeys: ['count'],
+                    labels: ['Count']
+                });
+                return xhr.responseText;
+            }
+        };
+    }
+    if ($("#allDate").attr('checked', 'checked')) {
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:3000/api/gotourl/infourlsalldata"); // async=true
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.send(jsonData);
+        xhr.onload = function (e) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                let graphData = JSON.parse(xhr.responseText);
+                Morris.Bar({
+                    element: 'bar-urls',
+                    data: graphData,
+                    xkey: 'url',
+                    ykeys: ['count'],
+                    labels: ['Count']
+                });
+                return xhr.responseText;
+            }
+        };
+    }
 }
 
 $(document).ready(() => {
